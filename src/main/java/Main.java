698caf320618dff1182b8edd0d9d955d589eb8e7
ganeshmerugu <80976048+ganeshmerugu@users.noreply.github.com>
2024-05-13 17:32:04 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,131 +1,74 @@
-import javax.swing.*;
 import java.io.*;
 import java.util.*;
-public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println(args[0]);
-        FileReader fr1 = new FileReader("/Users/ganes/input_1.csv");
-        FileReader fr2 = new FileReader("/Users/ganes/input_2.csv");
-        BufferedReader br1 = new BufferedReader(fr1);
-        BufferedReader br2 = new BufferedReader(fr2);
-        String heading1 = br1.readLine();
-        String heading2 = br2.readLine();
-        String[] head1 = heading1.split(",", 4);
-        String[] head2 = heading2.split(",", 4);
-        List<String[]> list1 = new ArrayList<>();
-        List<String[]> list2 = new ArrayList<>();
-        String str1 = "";
-        while ((str1 = br1.readLine()) != null) {
-            list1.add(str1.split(",", 4));
-        }
-        br1.close();
-        fr1.close();
-        str1 = "";
-        while ((str1 = br2.readLine()) != null) {
-            list2.add(str1.split(",", 4));
-        }
-        br2.close();
-        fr2.close();
+import java.util.concurrent.*;
 
-        Map<String, Integer> hmap1 = new HashMap<>();
-        Map<String, Integer> hmap2 = new HashMap<>();
+public class ReconciliationApplication {
+    private static final int NUM_THREADS = 4; 
 
-        for (int i = 0; i < list1.size(); i++) {
-            String[] temp = list1.get(i);
-            hmap1.put(temp[0], i);
-        }
-        for (int i = 0; i < list2.size(); i++) {
-            String[] temp = list2.get(i);
-            hmap2.put(temp[0], i);
-        }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String filePath1 = "/path/to/input/file1.csv";
+        String filePath2 = "/path/to/input/file2.csv";
 
-        FileWriter fw = new FileWriter("/Users/saiprasad/final_01.csv");
-        BufferedWriter bw = new BufferedWriter(fw);
-        int index1 = 0;
-        int index2 = 0;
-        for (Map.Entry<String, Integer> map : hmap1.entrySet()) {
-            String temp1 = map.getKey();
-            index1 = map.getValue();
-            if (hmap2.get(temp1) != null) {
-                index2 = hmap2.get(temp1);
-                hmap2.remove(temp1);
-                bw.write(temp1);
-                bw.write(",");
+        String outputFile = "/path/to/output/reconciled_output.csv";
 
-                String[] curr1 = list1.get(index1);
-                String[] curr2 = list2.get(index2);
-                for (int i = 1; i < 4; i++) {
-
-                    if (curr1[i] != null && curr2[i] != null) {
-                        bw.write(curr1[i]);
-                        if (i < 2)
-                            bw.write(',');
-                    } else if (curr1[i] == null && curr2[i] != null) {
-                        bw.write(curr2[i]);
-                        bw.write("(f1)");
-                        if (i < 2)
-                            bw.write(',');
-                    } else if (curr1[i] != null && curr2[i] == null) {
-                        bw.write(curr1[i]);
-                        bw.write("(f2)");
-                        if (i < 2)
-                            bw.write(',');
-                    }
-
-
-                }
-
-            } else {
-                bw.write(temp1);
-                bw.write("(f2)");
-                bw.write(",");
-
-                String[] curr1 = list1.get(index1);
-                for (int i = 1; i < 4; i++) {
-
-                    if (curr1[i] != null) {
-                        bw.write(curr1[i]);
-                        if (i < 2)
-                            bw.write(',');
-                    } else {
-                        bw.write("(f1)(f2)");
-                        if (i < 2)
-                            bw.write(',');
-                    }
-
-
-                }
-
-            }
-            bw.newLine();
-        }
-        for (Map.Entry<String, Integer> map : hmap2.entrySet()) {
-            String temp1 = map.getKey();
-            index1 = map.getValue();
-            bw.write(temp1);
-            bw.write("(f1)");
-            bw.write(",");
-            String[] curr1 = list2.get(index1);
-            for (int i = 1; i < 4; i++) {
-
-                if (curr1[i] != null) {
-                    bw.write(curr1[i]);
-                    if (i < 2)
-                        bw.write(',');
-                } else {
-                    bw.write("(f1)(f2)");
-                    if (i < 2)
-                        bw.write(',');
-                }
-            }
-            bw.newLine();
-        }
-      bw.close();
-      fw.close();
-
+        reconcileFiles(filePath1, filePath2, outputFile);
     }
 
+    private static void reconcileFiles(String filePath1, String filePath2, String outputFile) throws IOException, InterruptedException {
+        List<List<String[]>> chunks1 = partitionFile(filePath1);
+        List<List<String[]>> chunks2 = partitionFile(filePath2);
 
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+
+        List<Future<List<String[]>>> reconciliationTasks = new ArrayList<>();
+
+        for (int i = 0; i < chunks1.size(); i++) {
+            List<String[]> chunk1 = chunks1.get(i);
+            List<String[]> chunk2 = chunks2.get(i);
+            reconciliationTasks.add(executor.submit(() -> reconcileChunks(chunk1, chunk2)));
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+
+        List<String[]> reconciledData = new ArrayList<>();
+        for (Future<List<String[]>> task : reconciliationTasks) {
+            reconciledData.addAll(task.get());
+        }
+
+        writeReconciledData(reconciledData, outputFile);
+    }
+
+    private static List<String[]> reconcileChunks(List<String[]> chunk1, List<String[]> chunk2) {
+        return new ArrayList<>(chunk1);
+    }
+
+    private static List<List<String[]>> partitionFile(String filePath) throws IOException {
+      
+        List<List<String[]>> chunks = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            List<String[]> currentChunk = new ArrayList<>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                currentChunk.add(line.split(","));
+                if (currentChunk.size() >= 1000) { 
+                    chunks.add(new ArrayList<>(currentChunk));
+                    currentChunk.clear();
+                }
+            }
+            if (!currentChunk.isEmpty()) {
+                chunks.add(new ArrayList<>(currentChunk));
+            }
+        }
+        return chunks;
+    }
+
+    private static void writeReconciledData(List<String[]> reconciledData, String outputFile) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+            for (String[] record : reconciledData) {
+                writer.write(String.join(",", record));
+                writer.newLine();
+            }
+        }
+    }
 }
-
-
